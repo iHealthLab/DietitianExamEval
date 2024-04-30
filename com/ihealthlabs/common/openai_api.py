2e2ab52cc169617e7dd66270b62ada4e9d9665de
Chenyu Niu <106694844+Chenyu-iHealth@users.noreply.github.com>
2024-04-30 15:03:27 -0700

    add gpt3.5 for benchmark

--- com/ihealthlabs/common/openai_api.py
+++ com/ihealthlabs/common/openai_api.py
@@ -10,11 +10,11 @@ class OpenAIAPI(object):
     def __init__(self):
         openai.api_key = env.str("OPENAI_API_KEY")
 
-    def ask_chatgpt(self, question):
+    def ask_chatgpt(self, question, model_str):
         res = ""
         client = OpenAI()
         stream = client.chat.completions.create(
-            model="gpt-4",
+            model=model_str,
             messages=[{"role": "user", "content": question}],
             stream=True,
         )
@@ -30,7 +30,10 @@ if __name__ == '__main__':
     qsql = QuestionsMysql()
     question_dict = qsql.get_questions()
     prompt_str = qsql.get_prompt_string(question_dict)
-    response = api.ask_chatgpt(prompt_str)
-    print(response)
-    score = qsql.get_score(response, question_dict)
-    print(score)
+    response4 = api.ask_chatgpt(prompt_str, "gpt-4")
+    response35 = api.ask_chatgpt(prompt_str, "gpt-3.5-turbo")
+    score4 = qsql.get_score(response4, question_dict)
+    score35 = qsql.get_score(response35, question_dict)
+    print("\n")
+    print('gpt4 :' + score4)
+    print('gpt3.5 :' + score35)
