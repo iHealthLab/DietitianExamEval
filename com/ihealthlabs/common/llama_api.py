f3a95ce041bad7fc6dc87dd57a32acd457eee931
mohan-iHealth <mohan.qi@ihealthlabs.com>
2024-05-10 17:22:54 -0700

    Added Anthropic API and combined all three models connection into one file.

--- com/ihealthlabs/common/llama_api.py
+++ com/ihealthlabs/common/llama_api.py
@@ -2,10 +2,13 @@ from config import env
 from questions_mysql import QuestionsMysql
 
 import ollama
+import asyncio
+from anthropic import AsyncAnthropic
 
 qsql = QuestionsMysql()
 question_dict = qsql.get_questions()
 
+# Connect to ollama
 response = ollama.chat(model='llama3', messages=[
   {
     'role': 'user',
@@ -17,3 +20,26 @@ print(responsellama)
 scorellama = qsql.get_score(responsellama, question_dict)
 print("llama3: " + scorellama)
 
+'''
+# Connect to Anthropic Claude API
+client = AsyncAnthropic(api_key=env.str("ANTHROPIC_API_KEY"),)
+
+async def main() -> None:
+    async with client.messages.stream(
+        max_tokens=1024,
+        messages=[{"role": "user", "content": qsql.get_prompt_string(question_dict)}],
+        # messages=[{"role": "user", "content": "Why sky is blue?"}],
+        model="claude-3-opus-20240229",
+        # stream=True,
+    ) as stream:
+        async for text in stream.text_stream:
+            print(text, end="", flush=True)
+        print()
+    # print(message.content)
+    res = await stream.get_final_text()
+
+    scoreClaude = qsql.get_score(res, question_dict)
+    print("Claude 3 Opus: " + scoreClaude)
+
+asyncio.run(main())
+'''
