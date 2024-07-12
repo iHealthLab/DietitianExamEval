91e1b3fa14fdbb9202770c325306df97d72579a2
mohan-iHealth <mohan.qi@ihealthlabs.com>
2024-07-12 16:19:54 -0700

    Finished with SC rounds and result processing

--- com/ihealthlabs/common/check_missing_answers.py
+++ com/ihealthlabs/common/check_missing_answers.py
@@ -6,13 +6,17 @@ def check_missing_answers(filename):
     missing_answers = []
     
     for i, question in enumerate(questions[1:], start=1):  # start=1 to skip the first empty split
-        if '<answer>' not in question or '</answer>' not in question:
+        # if '<answer>' not in question or '</answer>' not in question:
+        #if '</answer>' not in question:
+            #missing_answers.append(i)
+        answer_count = question.count('</answer>')
+        if answer_count == 1:
             missing_answers.append(i)
     
     return missing_answers
 
 # Usage
-filename = 'gemini_1.5_pro_cot_exp1 copy 2.txt'  # Replace with your file name
+filename = 'gemini_1.5_pro_sc_cot_exp15.txt'  # Replace with your file name
 missing_answers = check_missing_answers(filename)
 
 if missing_answers:
